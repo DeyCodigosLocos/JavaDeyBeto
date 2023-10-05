799ac2954a7ec1d3d83cf7563d4956886f374dfd
BetilloDelProyecto <albalvarez@estudiantec.cr>
2023-10-04 22:14:04 -0600

    prueba

---
+++ src/Clases/Defensa.java
@@ -0,0 +1,57 @@
+package Clases;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Defensa extends Personaje{
+    private int velocidad;
+    private JLabel label;
+    private ThreadZombie objetivo;
+
+    public Defensa(JLabel label,String nombre, String tipo, int alcance, int nivel, int nivelAparicion, int espacios, int danoPorsegundo, int vida, int posX, int posY) {
+        super(nombre, tipo, alcance, nivel, nivelAparicion, espacios, danoPorsegundo, vida, posX, posY);
+        this.velocidad = velocidad;
+        this.label = label;
+        this.objetivo = null;
+    }
+
+    public ThreadZombie getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(ThreadZombie objetivo) {
+        this.objetivo = objetivo;
+    }
+    
+    
+    public JLabel getLabel() {
+        return label;
+    }
+
+    public void setLabel(JLabel label) {
+        this.label = label;
+    }
+    
+    public ThreadZombie getCloserZombie(ArrayList <ThreadZombie> zombies){
+        for (int i = 1; i <= this.getAlcance(); i++) {
+            ArrayList<Point> puntos = setPossibleMoves(i);
+            for (int j = 0; j < puntos.size(); j++) {
+                for (int k = 0; k < zombies.size(); k++) {
+                    ThreadZombie zombie = zombies.get(k);
+                    //System.out.println("ZOMBIE X:  " + zombie.getZombie().getPosX() + "  ZOMBIE Y :  " + zombie.getZombie().getPosY());
+                    System.out.println("X:  " + puntos.get(j).x + "  Y:  " + puntos.get(j).y );
+                    if(zombie.getZombie().getPosX() == puntos.get(j).x && zombie.getZombie().getPosY() == puntos.get(j).y)
+                        return zombie;
+                }
+            }
+        } 
+        return null;
+    }
+
+    public void atacarZombie(){
+        this.getObjetivo().getZombie().setVida(this.getObjetivo().getZombie().getVida()-this.getDanoPorSegundo());
+    }
+    
+}
---
+++ src/Clases/ThreadDefensa.java
@@ -0,0 +1,83 @@
+package Clases;
+
+import GUI.CampoDeBatalla;
+import static java.lang.Thread.sleep;
+
+/**
+ *
+ * @author X
+ */
+public class ThreadDefensa extends Thread{
+    CampoDeBatalla ventana;
+    boolean isRunning = true;
+    Defensa defensa;
+    //boolean isPaused = false;
+    
+    public ThreadDefensa(Defensa defensa, CampoDeBatalla refVentana) {
+        this.defensa = defensa; 
+        this.ventana = refVentana;
+    }
+    
+    @Override
+    public void run() {
+        while(isRunning){
+            try {
+                
+                //System.out.println("entro al try");
+                if (defensa.getObjetivo() == null || defensa.getObjetivo().getZombie().isDead()){
+                    //System.out.println("entro al if 1");
+                    defensa.setObjetivo(defensa.getCloserZombie(ventana.getZombies()));
+                }else{
+                    defensa.atacarZombie();
+                    defensa.getObjetivo().getZombie().getLabel().setText(defensa.getObjetivo().getZombie().getVida()+"");
+                    if(defensa.getObjetivo().getZombie().isDead()){
+                        defensa.getObjetivo().getZombie().morir();
+                        defensa.getObjetivo().isRunning = false;
+                        //System.out.println(defensa.getObjetivo().getZombie().getPosX() + ", "+ defensa.getObjetivo().getZombie().getPosY());
+                        ventana.cambiarImagenDeLabel("imgs//zombieMuerto.png",defensa.getObjetivo().getZombie().getLabel());
+                    }
+                        
+                }
+                sleep(1000);
+            } catch (InterruptedException ex) {
+                
+            }
+            
+        } 
+    }
+
+    public CampoDeBatalla getVentana() {
+        return ventana;
+    }
+
+    public void setVentana(CampoDeBatalla ventana) {
+        this.ventana = ventana;
+    }
+
+    public boolean isIsRunning() {
+        return isRunning;
+    }
+
+    public void setIsRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public Defensa getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(Defensa defensa) {
+        this.defensa = defensa;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreadDefensa{" + "defensa=" + defensa + '}';
+    }
+
+  
+    
+    
+    
+}
+
